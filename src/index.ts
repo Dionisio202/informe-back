6b5aee11926b2437a24d5600d4c8f749615da2f9
dionisio202 <solisedison@outlook.com>
2025-02-03 15:07:48 -0500

    added routes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
+import fs from 'fs';
+import path from 'path';
 const cors = require("cors");
 // Importar eventos
 const authEvents = require('./events/auth');
@@ -47,6 +49,44 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         console.log(`Cliente desconectado: ${socket.id}`);
     });
+});
+
+// Ruta para obtener un documento
+app.get('/api/document', (req, res) => {
+  const filePath = path.join(__dirname, 'documents', 'test-document.docx');
+  if (fs.existsSync(filePath)) {
+    res.sendFile(filePath);
+    console.log('Documento enviado correctamente y d');
+  } else {
+    res.status(404).send('El documento no existe');
+  }
+});
+
+// Ruta para guardar un documento
+app.post('/api/save-document', (req, res) => {
+console.log('Datos recibidos:', req.body);  // Agrega esta línea para inspeccionar los datos
+const filePath = path.join(__dirname, 'documents', 'saved-document.docx');
+const documentData = req.body;
+
+if (documentData?.url) {
+    const file = fs.createWriteStream(filePath);
+    const request = http.get(documentData.url, (response) => {
+        response.pipe(file);
+        file.on('finish', () => {
+            file.close(() => {
+                console.log('Documento guardado correctamente');
+                res.status(200).send('Documento guardado correctamente');
+            });
+        });
+    });
+
+    request.on('error', (err) => {
+        console.error('Error al descargar el documento:', err);
+        res.status(500).send('Error al guardar el documento');
+    });
+} else {
+    res.status(400).send('Datos inválidos recibidos');
+}
 });
 
 // Iniciar el servidor
